e10b9b52a6e5a767979afeeabe634f61c1dc87e3
Greg Vondra <gvondra@gmail.com>
2022-02-28 16:15:33 -0600

    add thread control around file purge

--- Interface.User/Data/FilePurger.cs
+++ Interface.User/Data/FilePurger.cs
@@ -3,22 +3,33 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Keesh.Interface.User.Data
 {
     public class FilePurger : IFilePurger
     {        
+        private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1,1);
+
         // expiration timestamp represents the time before which files are considered expired
         public async Task Purge(DateTime expirationTimestamp, IEnumerable<string> fileNames)
         {
             expirationTimestamp = expirationTimestamp.ToUniversalTime();
-            await Task.Run(() =>
+            await Task.Run(async () =>
             {
-                foreach (string fileName in fileNames.Where(f => File.Exists(f)))
+                await _semaphore.WaitAsync();
+                try
                 {
-                    if (File.GetLastWriteTimeUtc(fileName) < expirationTimestamp)
-                        File.Delete(fileName);
+                    foreach (string fileName in fileNames.Where(f => File.Exists(f)))
+                    {
+                        if (File.GetLastWriteTimeUtc(fileName) < expirationTimestamp)
+                            File.Delete(fileName);
+                    }
+                }
+                finally
+                {
+                    _semaphore.Release();
                 }
             });
         }
