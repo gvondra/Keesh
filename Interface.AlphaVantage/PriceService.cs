277dde2dc7518853030c4f2576991391b3a4ca35
Greg Vondra <gvondra@gmail.com>
2022-02-28 16:16:02 -0600

    add thread control structure to limit call rate

--- Interface.AlphaVantage/PriceService.cs
+++ Interface.AlphaVantage/PriceService.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
 
@@ -15,6 +16,7 @@ namespace Keesh.Interface.AlphaVantage
 {
     public class PriceService : IPriceService
     {
+        private readonly static SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
         private readonly IService _service;
 
         public PriceService(IService service)
@@ -24,18 +26,29 @@ namespace Keesh.Interface.AlphaVantage
 
         public async Task<List<PriceItem>> GetDaily(ISettings settings, string symbol, string apiKey, bool fullResultSet = false)
         {
-            string outputSize = "full";
-            if (!fullResultSet)
-                outputSize = "compact";
-            UriBuilder uriBuilder = new UriBuilder(settings.BaseAddress);
-            uriBuilder.Path = "query";
-            uriBuilder.Query = $"function=TIME_SERIES_DAILY&symbol={HttpUtility.UrlEncode(symbol)}&outputSize={outputSize}&datatype=csv&apikey={HttpUtility.UrlEncode(apiKey)}";
-            IRequest request = _service.CreateRequest(uriBuilder.Uri, HttpMethod.Get);
-            IResponse response = await _service.Send(request);
-            using (StreamReader reader = new StreamReader(await response.Message.Content.ReadAsStreamAsync()))
-            using (CsvReader csvReader = new CsvReader(reader, CultureInfo.CurrentCulture))
+            await _semaphore.WaitAsync();
+            try
             {
-                return (csvReader.GetRecords<PriceItem>()).ToList();
+                // the alpha vantage web site is rate limited as 5 calls per minute
+                Thread.Sleep(13000);
+                string outputSize = "full";
+                if (!fullResultSet)
+                    outputSize = "compact";
+                UriBuilder uriBuilder = new UriBuilder(settings.BaseAddress);
+                uriBuilder.Path = "query";
+                uriBuilder.Query = $"function=TIME_SERIES_DAILY&symbol={HttpUtility.UrlEncode(symbol)}&outputSize={outputSize}&datatype=csv&apikey={HttpUtility.UrlEncode(apiKey)}";
+                IRequest request = _service.CreateRequest(uriBuilder.Uri, HttpMethod.Get);
+                IResponse response = await _service.Send(request);
+                //System.Diagnostics.Debug.WriteLine(await response.Message.Content.ReadAsStringAsync());
+                using (StreamReader reader = new StreamReader(await response.Message.Content.ReadAsStreamAsync()))
+                using (CsvReader csvReader = new CsvReader(reader, CultureInfo.CurrentCulture))
+                {
+                    return (csvReader.GetRecords<PriceItem>()).ToList();
+                }
+            }
+            finally
+            {
+                _semaphore.Release();
             }
         }
     }
